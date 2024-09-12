7a3bc6d22f9b8e20436a79856cce9593e7c01e21
Mafumu12 <mafumumumba12112@gmail.com>
2024-09-12 16:37:19 +0200

    init 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Overall class to manage game assests and behavior."""
@@ -12,6 +13,7 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
    
 
@@ -21,17 +23,40 @@ class AlienInvasion:
 
         while True:
 
-            # Watch for Keyboard and mouse events.
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                # Watch for Keyboard and mouse events.
+                self._check_events()
+                self._update_screen()
+
+     
+              
+
+
+    def _update_screen(self):
+         """Update images on the screen, and flip to the new screen."""
+
+         # Redraw the screen during each pass through the loop.
+         self.screen.fill(self.settings.bg_color)
+         self.ship.blitme()
+
+        #Make the most recently drawn screen visible.
+         pygame.display.flip()
+
+
+    
+    def _check_events(self):
+     
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               
                     sys.exit()
+          
+   
 
-                # Redraw the screen during each pass through the loop.
-                self.screen.fill(self.settings.bg_color)
+    
+            
 
-                #Make the most recently drawn screen visible.
-                pygame.display.flip()
 
 if __name__ == '__main__':
 
